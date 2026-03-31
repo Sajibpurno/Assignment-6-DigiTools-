2bcfc04c6ad10689e0ea8124473f28dc24c2d072
Sajibpurno <chasajib86@gmail.com>
2026-04-01 01:09:26 +0600

    solve the error and add loading now complete assignment

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ThreeStep from './Components/ThreeStep'
 import ProductCard from './Components/Card/ProductCard'
 import { useState } from 'react'
 import Footer from './Components/Footer'
+import { MutatingDots } from 'react-loader-spinner'
 
 function App() {
   const DigitalData = fetch('/Data.json').then(res => res.json())
@@ -24,7 +25,21 @@ function App() {
     <main>
       <Mid></Mid>
 
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={
+        <div className="flex justify-center items-center min-h-[50vh] w-full">
+  <MutatingDots
+    visible={true}
+    height="100"
+    width="100"
+    color="#4F39F6" 
+    secondaryColor="#9514FA" 
+    radius="12.5"
+    ariaLabel="mutating-dots-loading"
+    wrapperStyle={{}}
+    wrapperClass=""
+  />
+</div>
+      }>
       <ProductCard DigitalData={DigitalData} product={product} setProduct={setProduct} cart={cart} setCart={setCart}
 ></ProductCard>
       </Suspense>
